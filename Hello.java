a9cb0541fd7e8aa146fcf42cd57f2592fa66db19
Kosall <89934511+Kosall@users.noreply.github.com>
2024-05-02 16:00:46 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ public class Hi{
 	public static void main(String[]args){
 	System.err.println("Hello world");
 	int x=0;
+	for(int i=0;i<7;i++){
+		x++;
+
+		System.out.println("sum of x"+x);
+	}
 
 }
 
